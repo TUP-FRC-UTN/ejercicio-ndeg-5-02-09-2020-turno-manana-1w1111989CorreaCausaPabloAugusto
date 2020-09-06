83573a5a65e5acaa28d63e3656746ef402113506
1w1111989CorreaCausaPabloAugusto <69556999+1w1111989CorreaCausaPabloAugusto@users.noreply.github.com>
2020-09-06 17:58:57 -0300

    Problema 1.5
    Hice algunas modificaciones para que se habiliten y desahbiliten los botones. Por ejem,plo, si ya calculo un subtotal, se desabilita el boton "subtotal"  y se habilita nuevamente si cargamos un articulo nuevo.

---
+++ Problema 1.5/frmProductos.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Problema_1._5
+{
+    partial class frmProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstNombre = new System.Windows.Forms.ListBox();
+            this.lstPrecio = new System.Windows.Forms.ListBox();
+            this.lstCantidad = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstSubtotal = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnSubtotal = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(100, 44);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(100, 81);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 1;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(100, 118);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 2;
+            this.txtCantidad.TextChanged += new System.EventHandler(this.txtCantidad_TextChanged);
+            this.txtCantidad.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtCantidad_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Cantidad";
+            // 
+            // lstNombre
+            // 
+            this.lstNombre.FormattingEnabled = true;
+            this.lstNombre.Location = new System.Drawing.Point(230, 44);
+            this.lstNombre.Name = "lstNombre";
+            this.lstNombre.Size = new System.Drawing.Size(120, 95);
+            this.lstNombre.TabIndex = 6;
+            // 
+            // lstPrecio
+            // 
+            this.lstPrecio.FormattingEnabled = true;
+            this.lstPrecio.Location = new System.Drawing.Point(356, 44);
+            this.lstPrecio.Name = "lstPrecio";
+            this.lstPrecio.Size = new System.Drawing.Size(120, 95);
+            this.lstPrecio.TabIndex = 7;
+            // 
+            // lstCantidad
+            // 
+            this.lstCantidad.FormattingEnabled = true;
+            this.lstCantidad.Location = new System.Drawing.Point(498, 44);
+            this.lstCantidad.Name = "lstCantidad";
+            this.lstCantidad.Size = new System.Drawing.Size(58, 95);
+            this.lstCantidad.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Precio";
+            // 
+            // lstSubtotal
+            // 
+            this.lstSubtotal.FormattingEnabled = true;
+            this.lstSubtotal.Location = new System.Drawing.Point(571, 44);
+            this.lstSubtotal.Name = "lstSubtotal";
+            this.lstSubtotal.Size = new System.Drawing.Size(58, 95);
+            this.lstSubtotal.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nombre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(367, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Precio";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(568, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Subtotal";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(495, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Cantidad";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(112, 158);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 39);
+            this.btnCargar.TabIndex = 3;
+            this.btnCargar.Text = "Cargar Producto";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnSubtotal
+            // 
+            this.btnSubtotal.Location = new System.Drawing.Point(554, 159);
+            this.btnSubtotal.Name = "btnSubtotal";
+            this.btnSubtotal.Size = new System.Drawing.Size(75, 38);
+            this.btnSubtotal.TabIndex = 4;
+            this.btnSubtotal.Text = "Calcular Subtotal";
+            this.btnSubtotal.UseVisualStyleBackColor = true;
+            this.btnSubtotal.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(554, 225);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 36);
+            this.btnTotal.TabIndex = 5;
+            this.btnTotal.Text = "Calcular Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(423, 286);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Total de la Compra: ";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtTotal.Location = new System.Drawing.Point(539, 279);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(90, 20);
+            this.txtTotal.TabIndex = 15;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(112, 276);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 19;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 332);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnSubtotal);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.lstSubtotal);
+            this.Controls.Add(this.lstCantidad);
+            this.Controls.Add(this.lstPrecio);
+            this.Controls.Add(this.lstNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "frmProductos";
+            this.Text = "Facturacion de Productos";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtCantidad_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstNombre;
+        private System.Windows.Forms.ListBox lstPrecio;
+        private System.Windows.Forms.ListBox lstCantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstSubtotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnSubtotal;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Problema 1.5/frmProductos.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema_1._5
+{
+    public partial class frmProductos : Form
+    {
+        double subTotal, total;
+        public frmProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < lstPrecio.Items.Count; i++)
+            {
+
+                lstSubtotal.Items.Add(Convert.ToString(Convert.ToDouble(lstPrecio.Items[i]) * Convert.ToDouble(lstCantidad.Items[i])));
+            }
+     
+            btnSubtotal.Enabled = false;
+
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos())
+            {
+                this.lstNombre.Items.Add(txtNombre.Text);
+                this.lstPrecio.Items.Add(txtPrecio.Text);
+                this.lstCantidad.Items.Add(txtCantidad.Text);
+                txtNombre.Clear();
+                txtPrecio.Clear();
+                txtCantidad.Clear();
+                txtNombre.Focus();
+                btnSubtotal.Enabled = true;
+                lstSubtotal.Items.Clear();
+            }
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < lstSubtotal.Items.Count; i++)
+            {
+                total += Convert.ToDouble(lstSubtotal.Items[i]);
+            }
+            txtTotal.Text = Convert.ToString(total);
+            lstSubtotal.Items.Clear();
+            lstNombre.Items.Clear();
+            lstPrecio.Items.Clear();
+            lstCantidad.Items.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult opcion = MessageBox.Show("Está seguro que desea salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (opcion == DialogResult.Yes)
+                Close();
+        }
+        private void TxtCantidad_KeyDown(object sender, KeyEventArgs e)
+        {
+            {
+                if (e.KeyCode == Keys.Enter) btnCargar.PerformClick();
+            }
+        }
+        private void txtCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool ValidarCampos()
+        {
+            if (String.IsNullOrEmpty(txtNombre.Text))
+            {
+                MessageBox.Show("Ingrese el nombre del producto", "Atencion!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtNombre.Text = "";
+                txtNombre.Focus();
+                return false;
+            }
+            if (String.IsNullOrEmpty(txtPrecio.Text))
+            {
+                MessageBox.Show("Ingrese el precio del producto", "Atencion!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtPrecio.Text = "";
+                txtPrecio.Focus();
+                return false;
+            }
+            if (String.IsNullOrEmpty(txtCantidad.Text))
+            {
+                MessageBox.Show("Ingrese la cantidad de unidades del producto", "Atencion!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                txtCantidad.Text = "";
+                txtCantidad.Focus();
+                return false;
+            }
+            return true;
+        }
+    }
+}
